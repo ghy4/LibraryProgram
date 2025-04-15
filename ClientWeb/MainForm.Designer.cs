0f93be763bdaf976b228a1573c12b396cc40b5e9
ghy4 <gbruzha2@gmail.com>
2025-04-15 22:25:18 +0300

    Placed controllers and updated main page; added library selection and book display for selected library

--- ClientWeb/MainForm.Designer.cs
+++ ClientWeb/MainForm.Designer.cs
@@ -28,20 +28,72 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			menuStrip1 = new MenuStrip();
+			LibraryMenuComboBox = new ToolStripComboBox();
+			SearchMenuItem = new ToolStripMenuItem();
+			MainPagePanel = new Panel();
+			menuStrip1.SuspendLayout();
 			SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			menuStrip1.ImageScalingSize = new Size(20, 20);
+			menuStrip1.Items.AddRange(new ToolStripItem[] { LibraryMenuComboBox, SearchMenuItem });
+			menuStrip1.Location = new Point(0, 0);
+			menuStrip1.Name = "menuStrip1";
+			menuStrip1.Size = new Size(1182, 40);
+			menuStrip1.TabIndex = 0;
+			menuStrip1.Text = "menuStrip1";
+			// 
+			// LibraryMenuComboBox
+			// 
+			LibraryMenuComboBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			LibraryMenuComboBox.Name = "LibraryMenuComboBox";
+			LibraryMenuComboBox.Size = new Size(160, 36);
+			LibraryMenuComboBox.SelectedIndexChanged += LibraryMenuComboBox_SelectedIndexChanged;
+			// 
+			// SearchMenuItem
+			// 
+			SearchMenuItem.BackColor = SystemColors.ControlLight;
+			SearchMenuItem.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+			SearchMenuItem.Margin = new Padding(2);
+			SearchMenuItem.Name = "SearchMenuItem";
+			SearchMenuItem.Size = new Size(83, 32);
+			SearchMenuItem.Text = "Search";
+			SearchMenuItem.Click += SearchMenuItem_Click;
+			// 
+			// MainPagePanel
+			// 
+			MainPagePanel.AutoScroll = true;
+			MainPagePanel.Location = new Point(0, 43);
+			MainPagePanel.Name = "MainPagePanel";
+			MainPagePanel.Size = new Size(1182, 707);
+			MainPagePanel.TabIndex = 1;
 			// 
 			// MainPage
 			// 
 			AutoScaleDimensions = new SizeF(8F, 20F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(1182, 753);
+			Controls.Add(MainPagePanel);
+			Controls.Add(menuStrip1);
 			FormBorderStyle = FormBorderStyle.FixedSingle;
+			MainMenuStrip = menuStrip1;
 			Name = "MainPage";
 			ShowIcon = false;
 			Text = "MainPage";
+			Load += MainPage_Load;
+			menuStrip1.ResumeLayout(false);
+			menuStrip1.PerformLayout();
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private MenuStrip menuStrip1;
+		private ToolStripComboBox LibraryMenuComboBox;
+		private ToolStripMenuItem SearchMenuItem;
+		private Panel MainPagePanel;
 	}
 }
--- ClientWeb/MainForm.cs
+++ ClientWeb/MainForm.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Server.DTOModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +16,81 @@ namespace ClientWeb
 {
 	public partial class MainPage : Form
 	{
+		private HttpClient _httpClient;
 		public MainPage()
 		{
 			InitializeComponent();
+			_httpClient = new HttpClient { BaseAddress = new Uri("https://localhost:7221") };
+		}
+		private async void MainPage_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				var response = await _httpClient.GetAsync("/api/Library/Libraries");
+				response.EnsureSuccessStatusCode();
+
+
+				var content = await response.Content.ReadAsStreamAsync();
+				var libraries = await JsonSerializer.DeserializeAsync<List<LibraryDTO>>(content, new JsonSerializerOptions
+				{
+					PropertyNameCaseInsensitive = true
+				});
+
+				foreach (var item in libraries)
+					LibraryMenuComboBox.Items.Add(item);
+				LibraryMenuComboBox.SelectedIndex = 0;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error: " + ex.Message);
+			}
+		}
+
+		private async void LibraryMenuComboBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var response = await _httpClient.GetAsync("/api/Library/Libraries");
+			response.EnsureSuccessStatusCode();
+
+
+			var content = await response.Content.ReadAsStreamAsync();
+			var libraries = await JsonSerializer.DeserializeAsync<List<LibraryDTO>>(content, new JsonSerializerOptions
+			{
+				PropertyNameCaseInsensitive = true
+			});
+			int y = 10;
+			MainPagePanel.Controls.Clear();
+			foreach (var item in libraries[LibraryMenuComboBox.SelectedIndex].Books)
+			{
+				Label label = new Label();
+				label.Text = $"Title: {item.Title}";
+				label.Font = new Font("Segoe UI", 11, FontStyle.Regular);
+				label.AutoSize = true;
+				label.Location = new Point(10, y);
+				y += label.Height + 10;
+				Label label1 = new Label();
+				label1.Text = $"Author: {item.Author}";
+				label1.Font = new Font("Segoe UI", 11, FontStyle.Regular);
+				label1.AutoSize = true;
+				label1.Location = new Point(10, y);
+				y += label.Height + 10;
+				Label label2 = new Label();
+				label2.Text = $"Description: {item.Description}";
+				label2.Font = new Font("Segoe UI", 11, FontStyle.Regular);
+				label2.AutoSize = true;
+				label2.Location = new Point(10, y);
+				y += label.Height + 10;
+
+				MainPagePanel.Controls.Add(label);
+				MainPagePanel.Controls.Add(label1);
+				MainPagePanel.Controls.Add(label2);
+				y += label.Height + 10;
+			}
+		}
+
+		private void SearchMenuItem_Click(object sender, EventArgs e)
+		{
+			SearchForm form = new SearchForm(LibraryMenuComboBox.SelectedIndex);
+			form.Show();
 		}
 	}
 }
